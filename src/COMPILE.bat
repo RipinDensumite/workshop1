e6267a9b16f9027d32866108f46f63912eeb167c
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2026-06-03 22:44:53 +0800

    initial

--- src/COMPILE.bat
+++ src/COMPILE.bat
@@ -1,6 +1,6 @@
 @echo off
 
-g++ main.cpp login.cpp inventory.cpp StockMonitor.cpp ExpiryTrack.cpp Report.cpp db.cpp mainmenu.cpp -o app.exe
+g++ *.cpp -o app.exe
 
 if %errorlevel% neq 0 (
     echo Compile failed.
